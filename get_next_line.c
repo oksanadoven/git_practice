9451532918ff38e5542d08fef9a9ff8edfa05f24
Oksana Solodovnyk <osolodov@e1z2r6p10.42.us.org>
2020-03-10 16:06:54 -0700

    Added polished gnl files

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,24 @@
 /*   By: osolodov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 14:18:12 by osolodov          #+#    #+#             */
-/*   Updated: 2020/03/06 15:19:11 by osolodov         ###   ########.fr       */
+/*   Updated: 2020/03/10 13:03:40 by osolodov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* 
+/*
 ** Creates a new node of the linked list. The node will be created only in case
 ** a unique file descriptor is passed to the get_next_line() fn. The size of
 ** leftover str is reserved based on the BUFF_SIZE macro. It will be manipulated
 ** in the course of the program execution
 */
 
-node	*create_el(int fd)
+t_node	*create_el(int fd)
 {
-	node	*new;
-	if (!(new = (node *)malloc(sizeof(node))))
+	t_node	*new;
+
+	if (!(new = (t_node *)malloc(sizeof(t_node))))
 		return (NULL);
 	new->fd = fd;
 	new->leftover = ft_strnew(BUFF_SIZE);
@@ -30,12 +31,12 @@ node	*create_el(int fd)
 	return (new);
 }
 
-/* 
+/*
 ** If there's a leftover as a result of first time execution of the fn
 ** FIND_NL will 1) set new_line_point to the \n character in the leftover str
 ** 2) make a duplicate of the string from leftover till \n and put it in str
 ** 3) update leftover str by copying everything that remained after \n
-** 
+**
 ** If there's an empty leftover or there's no \n in leftover the fn
 ** will allocate a new str and append the contents of leftover to it. After
 ** that it will clear leftover (assuming that we reached EOF)
@@ -60,17 +61,17 @@ char	*find_nl(char *leftover, char **new_line_point)
 	return (str);
 }
 
-/* 
+/*
 ** Fn GET_LINE executes the main functionality of the program: first it calls
-** find_nl() fn which will pull the new line in case there was a leftover after 
+** find_nl() fn which will pull the new line in case there was a leftover after
 ** first execution of the program.
 **
-** If it is the first time execution and the result of the find_nl() call is 
-** empty lines, the function starts reading the fd using read() fn call and 
+** If it is the first time execution and the result of the find_nl() call is
+** empty lines, the function starts reading the fd using read() fn call and
 ** putting \0 either when it finished reading or if the BUFFER is full. Then it
 ** looks for a \n in the BUFFER and after it finds on, it copies everything
 ** after it into the leftover str and clears all the bytes before \n. After that
-** it attaches str in BUFFER (ending at new_line_point) to the *line str and 
+** it attaches str in BUFFER (ending at new_line_point) to the *line str and
 ** returns the code of execution.
 */
 
@@ -101,21 +102,21 @@ int		get_line(int fd, char **line, char *leftover)
 			|| ft_strlen(leftover)) ? READ_NL : READ_FINISHED);
 }
 
-/* 
+/*
 ** GET_NEXT_LINE fn mainly deals with assigning multiple file descriptors
-** to the corresponding node of the static linked list. If there's no list 
+** to the corresponding node of the static linked list. If there's no list
 ** it creates one, otherwise it traverses the nodes till fd given as a param
 ** matches the node->fd. If in the result of the traversal there was no node
 ** with a matching fd, it will create one and append it to the end of the list.
-** After it made sure the node with the needed fd exists, it calls get_line() 
+** After it made sure the node with the needed fd exists, it calls get_line()
 ** fn to get the line. Being static, the nodes with corresponding fd contain
 ** the leftover strings as the result of each execution with a given fd.
 */
 
 int		get_next_line(const int fd, char **line)
 {
-	static node		*head;
-	node			*tmp;
+	static t_node	*head;
+	t_node			*tmp;
 
 	if (fd < 0 || !line)
 		return (ERROR);
--- get_next_line.h
+++ get_next_line.h
@@ -1,23 +1,35 @@
-# ifndef GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osolodov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/10 12:59:33 by osolodov          #+#    #+#             */
+/*   Updated: 2020/03/10 15:12:40 by osolodov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include "libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
-# define BUFF_SIZE 4096
+# define BUFF_SIZE 1
 
 # define ERROR -1
 # define READ_FINISHED 0
 # define READ_NL 1
 
-int		get_next_line(const int fd,char **line);
+int				get_next_line(const int fd, char **line);
 
-typedef struct node
+typedef struct	s_node
 {
-	int			fd;
-	char		*leftover;
-	struct node	*next;
-}				node;
+	int				fd;
+	char			*leftover;
+	struct s_node	*next;
+}				t_node;
 
 #endif
